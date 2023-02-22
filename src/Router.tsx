13d9d5d86054c232a1c636f35f814a91a8d92474
jnorberto23 <joaovictor.norberto@outlook.com>
2023-02-22 15:32:04 -0300

    🚧work in Routes, Header and DefaultLayout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
-import { DefaultLayout } from './layouts/DefaultLayout'
+import { DefaultLayout } from './layouts/DefaultLayouts'
 export function Router() {
-  return (
-    <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/history" element={<History />} />
-      </Route>
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<DefaultLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/history" element={<History />} />
+            </Route>
+        </Routes>
+    )
 }
