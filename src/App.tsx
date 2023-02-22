8ad4d761888cdba9ae49848838759e9d10f40268
jnorberto23 <joaovictor.norberto@outlook.com>
2023-02-22 12:36:53 -0300

    🚧 work in react Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <Router />
       <GlobalStyle />
     </ThemeProvider>
   )
