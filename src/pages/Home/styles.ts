ba47ba5f39318d4ae7fa9c2354f83620382d152a
jnorberto23 <joaovictor.norberto@outlook.com>
2023-02-24 11:58:22 -0300

    ✨add full cycle

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const HomeContainer = styled.main`
   flex: 1;
+
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -14,6 +15,7 @@ export const HomeContainer = styled.main`
     gap: 3.5rem;
   }
 `
+
 export const FormContainer = styled.div`
   width: 100%;
   display: flex;
@@ -25,6 +27,7 @@ export const FormContainer = styled.div`
   font-weight: bold;
   flex-wrap: wrap;
 `
+
 const BaseInput = styled.input`
   background: transparent;
   height: 2.5rem;
@@ -35,7 +38,7 @@ const BaseInput = styled.input`
   padding: 0 0.5rem;
   color: ${(props) => props.theme['gray-100']};
 
-  &::focus {
+  &:focus {
     box-shadow: none;
     border-color: ${(props) => props.theme['green-500']};
   }
@@ -47,6 +50,7 @@ const BaseInput = styled.input`
 
 export const TaskInput = styled(BaseInput)`
   flex: 1;
+
   &::-webkit-calendar-picker-indicator {
     display: none !important;
   }
@@ -61,6 +65,7 @@ export const CountdownContainer = styled.div`
   font-size: 10rem;
   line-height: 8rem;
   color: ${(props) => props.theme['gray-100']};
+
   display: flex;
   gap: 1rem;
 
@@ -70,36 +75,51 @@ export const CountdownContainer = styled.div`
     border-radius: 8px;
   }
 `
-
 export const Separator = styled.div`
   padding: 2rem 0;
   color: ${(props) => props.theme['green-500']};
+
   width: 4rem;
   overflow: hidden;
   display: flex;
   justify-content: center;
 `
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
-  border: none;
+  border: 0;
   padding: 1rem;
-  border-radius: 8ox;
+  border-radius: 8px;
+
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 8px;
+  color: ${(props) => props.theme['gray-100']};
+
+  gap: 0.5rem;
   font-weight: bold;
+
   cursor: pointer;
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
   background: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme['gray-100']};
 
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
   }
+`
 
-  &:disabled {
-    opacity: 0.7;
-    cursor: not-allowed;
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
   }
 `
